448f3007d6b74cd8bf64897de8a4055e16f0b562
Zsolt Deak <zsde01@betssongroup.com>
2020-08-27 13:30:49 +0200

    replace jira ticket number in title and description

---
+++ content.js
@@ -0,0 +1,22 @@
+const ISSUE_REGEX = /[a-zA-Z]{1,5}-\d{1,4}/g;
+const ISSUE_REGEX_DESCRIPTION = /Ref[:]?[\s]?#[\s]?[a-zA-Z]{1,5}-\d{1,4}/g;
+
+function replaceIssue(element, regex) {
+	const match = element.html().match(regex);
+	if (!match) return;
+	const issueId = match[0].match(ISSUE_REGEX);
+	const newIssueHtml = `<a href="https://edgealpha.corpsson.com/browse/${issueId}">${issueId}</a>`;
+	const replaceHtml = match[0].replace(ISSUE_REGEX, newIssueHtml);
+	const newElementHtml = element.html().replace(regex, replaceHtml);
+	element.html(newElementHtml);
+}
+
+(function convertTitle() {
+	const title = $('.js-issue-title');
+	replaceIssue(title, ISSUE_REGEX);
+})();
+
+(function convertDescription() {
+	const description = $('.js-comment-body').first();
+	replaceIssue(description, ISSUE_REGEX_DESCRIPTION);
+})();
